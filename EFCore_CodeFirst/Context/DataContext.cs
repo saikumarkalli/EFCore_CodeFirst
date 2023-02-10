7383b7abed274b48e496fa1434fd1a08199107b7
Kalli Sai Kumar <saikumar.kalli@peopletech.com>
2023-02-10 17:57:05 +0530

    changes

--- EFCore_CodeFirst/Context/DataContext.cs
+++ EFCore_CodeFirst/Context/DataContext.cs
@@ -6,35 +6,22 @@ namespace EFCore_CodeFirst.Context
 {
     public class DataContext : DbContext
     {
-        private  const string _connectionString = "Data Source=L201412;Initial Catalog=Pratice;Integrated Security=True;TrustServerCertificate=True";
-        //public DataContext()
-        //{
+        public DataContext() { }
 
-        //}
-        //public DataContext(): base(_connectionString)
-        //{
-
-        //}
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
 
-            optionsBuilder.UseSqlServer(_connectionString);
-        }
+        //    optionsBuilder.UseSqlServer(_connectionString);
+        //}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //PostgreSQL
-            //modelBuilder.HasDefaultSchema("public");
-            //modelBuilder.Entity<Employee>().ToTable("Employee");
-            //base.OnModelCreating(modelBuilder);
 
-            //SQL
-           // modelBuilder.Entity<StateMaster>().ToTable("StateMaster");
         }
 
         public DbSet<Employee> Employees { get; set; }
